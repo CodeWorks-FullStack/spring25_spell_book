b8ed33b96fd17e3ca263572a23cd8f8f9d912cc1
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-05 10:48:46 -0700

    storing active spell

--- app/services/DNDSpellsService.js
+++ app/services/DNDSpellsService.js
@@ -1,11 +1,14 @@
 import { AppState } from "../AppState.js";
-import { Spell } from "../models/Spell.js";
+import { DetailedSpell, Spell } from "../models/Spell.js";
 import { dndApi } from "../utils/Axios.js"
 
 class DNDSpellsService {
   async getSpellDetails(spellIndex) {
     const response = await dndApi.get(`spells/${spellIndex}`)
     console.log('GOT SPELL DETAILS 🪄📡🧙‍♂️', response.data);
+    // NOTE we got a single object back from the API, so we we convert into a single class object
+    const spell = new DetailedSpell(response.data)
+    AppState.activeSpell = spell
   }
   async getSpells() {
     const response = await dndApi.get('spells')

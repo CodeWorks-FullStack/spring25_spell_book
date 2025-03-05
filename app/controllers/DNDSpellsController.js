9eea1c69bd168dc877efef1bb27e738060a95161
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-05 09:59:27 -0700

    drawing spells

--- app/controllers/DNDSpellsController.js
+++ app/controllers/DNDSpellsController.js
@@ -1,9 +1,19 @@
+import { AppState } from "../AppState.js"
 import { dndSpellsService } from "../services/DNDSpellsService.js"
 import { Pop } from "../utils/Pop.js"
 
 export class DNDSpellsController {
   constructor() {
+    AppState.on('dndSpells', this.drawSpells)
     this.getSpells()
+  }
+
+  drawSpells() {
+    const spells = AppState.dndSpells
+    let spellsContent = ''
+    spells.forEach(spell => spellsContent += spell.buttonTemplate)
+    const spellEment = document.getElementById('dndSpellsList')
+    spellEment.innerHTML = spellsContent
   }
 
   async getSpells() {

c3fa56d5cfc718946c07bcb701c09be9515fbe85
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-05 09:47:59 -0700

    storing spells in appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,7 +1,11 @@
+import { Spell } from './models/Spell.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
+
+  /**@type {Spell[]}*/
+  dndSpells = []
 
 }
 
--- app/services/DNDSpellsService.js
+++ app/services/DNDSpellsService.js
@@ -1,9 +1,13 @@
+import { AppState } from "../AppState.js";
+import { Spell } from "../models/Spell.js";
 import { dndApi } from "../utils/Axios.js"
 
 class DNDSpellsService {
   async getSpells() {
     const response = await dndApi.get('spells')
     console.log('GOT SPELLS 📡🪄', response.data);
+    const spells = response.data.results.map(pojo => new Spell(pojo))
+    AppState.dndSpells = spells
   }
 }
 

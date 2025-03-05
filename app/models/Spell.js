572a669afa3e29dee4735b44b5339902ae698b6a
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-05 09:54:25 -0700

    button template

--- app/models/Spell.js
+++ app/models/Spell.js
@@ -4,11 +4,21 @@ export class Spell {
     this.level = data.level
     this.index = data.index
   }
+
+  get buttonTemplate() {
+    return `
+    <div>
+      <button class="btn btn-cyan mb-2 w-100" title="See details about ${this.name}" type="button">
+        ${this.name}
+      </button>
+    </div>
+    `
+  }
 }
 
-const dataExample = {
-  "index": "acid-arrow",
-  "name": "Acid Arrow",
-  "level": 2,
-  "url": "/api/2014/spells/acid-arrow"
-}
+// const dataExample = {
+//   "index": "acid-arrow",
+//   "name": "Acid Arrow",
+//   "level": 2,
+//   "url": "/api/2014/spells/acid-arrow"
+// }
